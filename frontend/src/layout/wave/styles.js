df8445d7b82a6a0be955f371862620a57223f6f8
Anthony <anthony.vii27@gmail.com>
2021-09-15 10:41:21 -0300

    feat: frontend - added creators name

--- frontend/src/layout/wave/styles.js
+++ frontend/src/layout/wave/styles.js
@@ -4,10 +4,27 @@ export const WaveContainer = styled.section`
     width: 100%;
     height: 160px;
     display: flex;
-    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
 
-    svg {
-        border-bottom-left-radius: 10px;
-        border-bottom-right-radius: 10px;
+export const WaveCreatorsContainer = styled.div`
+    width: 100%;
+    height: 25px;
+    background: #0099EF;
+    display: flex;
+    justify-content: flex-end;
+
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+
+    span {
+        color: #E5E5E5;
+        font-size: 12px;
+        font-weight: 500;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        padding: 0 16px;
     }
+        
+    
 `
