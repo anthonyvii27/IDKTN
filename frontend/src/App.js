480af59c3089679a5129e23ba8dd46c60c0772a8
Anthony <anthony.vii27@gmail.com>
2021-10-16 14:29:15 -0300

    frontendsaco

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MainScreen from './layout/main';
 import Device from './layout/device';
@@ -7,19 +7,34 @@ import Bar from './layout/bar';
 import MessageSection from './layout/message';
 import Wave from './layout/wave';
 import RFIDReader from './layout/rfidReader';
+import { socket } from './services/client';
 
-const App = () => (
+const App = () => {
+    const [event, setEvent] = useState({
+        message: "Aproxime o cartão",
+        balance: 0
+    });
+
+    useEffect(() => {
+        socket.on("message", data => {
+            console.log(data);
+            // setEvent([data]);
+        });
+    }, [socket]);
+
+    return (
     <MainScreen>
         <Device>
             {/* #0DB628 */}
             <DeviceScreen bgColor="#00A3FF">
                 <Bar />
-                <MessageSection message="Aproxime o cartão" balance="" />
+                <MessageSection message={event.message} balance={event.balance} />
                 <Wave />
             </DeviceScreen>
             <RFIDReader />
         </Device>
     </MainScreen>
 );
+    }
 
 export default App;
